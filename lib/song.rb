585f8300dd075d4ade2de09984496d2c1b117679
Lenny Lee <lennylee91@gmail.com>
2019-11-24 21:45:21 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+  
+    @@count = 0
+    
+    
+     
+
+  def initialize(name, artist, genre)
+     
+     @name = name
+     @artist = artist
+     @genre = genre
+     @@count += 1
+     @@genres << genres
+     @@artists << artist
+      
+     
+  end
+  
+  
+  def self.count
+    @@count
+  end
+
+end
